7331b4f93f6699ced5c5ae50be3b2e118bbbfa4b
JDerenzin <7derenzin@gmail.com>
2026-04-02 19:25:19 -0500

    ajuste por mandado de angel

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const productos = [
-    { nombre: "Jardín de la Alegría", precio: "25.00", img: "img/jardin-de-la-alegria.webp", desc: "Ramo de miniflores amarillas + abejas + cartel de corazón" },
-    { nombre: "Cielo Eterno", precio: "15.00", img: "img/cielo-eterno.webp", desc: "1 tulipán azul en ramo" },
-    { nombre: "Rayo de Luz", precio: "35.00", img: "img/rayo-de-luz.webp", desc: "Girasol + 4 margaritas + 1 rama de eucalipto" },
-    { nombre: "Sol de Mediodía", precio: "8.00", img: "img/sol-de-mediodia.webp", desc: "3 girasoles + 8 margaritas + 2 ramas de eucalipto" },
-    { nombre: "Trilogía del Afecto", precio: "28.00", img: "img/trilogia-del-afecto.webp", desc: "3 tulipanes rojos con mensaje" },
-    { nombre: "Elegancia Silvestre", precio: "40.00", img: "img/elegancia-silvestre.webp", desc: "2 lirios rosados + 4 margaritas (versión girasol) + 2 eucaliptos" }
+    { nombre: "Ramo de Abejitas", precio: "35.00", img: "img/ramo-de-abejitas.webp" },
+    { nombre: "Tulipan Premium", precio: "12.00", img: "img/tulipan-premium.webp" },
+    { nombre: "Rayo de Luz", precio: "20.00", img: "img/rayo-de-luz.webp" },
+    { nombre: "Sol de Mediodía", precio: "30.00", img: "img/sol-de-mediodia.webp" },
+    { nombre: "I Loviu", precio: "30.00", img: "img/i-loviu.webp" },
+    { nombre: "Dulce Delirio", precio: "30.00", img: "img/dulce-delirio.webp" }
+    { nombre: "Te quiero de Forma Verdadera", precio: "40.00", img: "img/te-quiero-de-forma-verdadera.webp" }
 ];
 
 const container = document.getElementById('catalogo');
@@ -15,11 +16,7 @@ function render() {
             <img src="${p.img}" alt="${p.nombre}" loading="lazy">
             <div class="info">
                 <h3>${p.nombre}</h3>
-                <p>${p.desc}</p>
                 <div class="precio">S/${p.precio}</div>
-                <a href="https://www.instagram.com/taller_de_angel_/" class="btn-ig">
-                    Consultar disponibilidad
-                </a>
             </div>
         </div>
     `).join('');
